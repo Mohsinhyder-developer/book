3219375d9d821977d5418baa09bdcb883f22abec
Mohsin-Hyder <tm296895@gmail.com>
2025-02-06 20:12:26 +0500

    Your commit message here

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-'mongodb://localhost:27017/mystore'
+'mongodb+srv://davidthomas93452:mohsinhyder@cluster0.be5l5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
